e14b5b2459585885952011f9490a72a7ae3fa271
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-08 07:11:06 -0800

    unix is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int n = 0;
 
 	while (ch[n] != '\0')
