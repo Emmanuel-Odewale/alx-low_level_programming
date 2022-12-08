c08f2b70d3c58bbb52bf0ecf43248b6f6e702b86
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-08 07:08:49 -0800

    unix is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program's main function.
+ * Description: this is where program execution begins and ends.
+ * @void: accepts no param.
+ * printf: stdout function to output to the console.
+ * Return: return value for main function
+ */
+int main(void)
+{
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int n = 0;
+
+	while (ch[n] != '\0')
+	{
+		putchar(ch[n]);
+		n++;
+	}
+	return (1);
+}
