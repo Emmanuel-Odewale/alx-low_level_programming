01833a4bf902551859bd9297c90ca8c7efb7d941
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-08 05:19:59 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program's main function
+ * Description: the main function is where program execution begins
+ * @void: accepts no param
+ * printf - stdout function to display
+ * Return: return value for main function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
