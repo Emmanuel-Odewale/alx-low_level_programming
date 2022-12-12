eedbe48cb3ae2c7e313e9af9c0cfec0e0f1e8243
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-11 18:19:26 -0800

    when i was having that alphabet soup, i never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - program's main function.
+* Description: this is where program execution begins and ends.
+* @void: accepts no param.
+* Return: return value for main function
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
