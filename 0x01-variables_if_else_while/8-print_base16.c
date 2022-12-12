e322b0c99ee9325eced1c8d1f3dac02e25d3d214
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-11 19:29:22 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 16; i++)
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
-
 	return (0);
 }
