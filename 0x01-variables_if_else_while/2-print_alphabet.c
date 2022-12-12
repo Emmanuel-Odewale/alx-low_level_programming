688b56088261d4d8e23fdb6b48849ec31b85d4e4
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-11 17:41:25 -0800

    i sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - program's main function.
+* Description: this is where program execution begins and ends.
+* @void: accepts no param.
+* Return: return value for main function
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
