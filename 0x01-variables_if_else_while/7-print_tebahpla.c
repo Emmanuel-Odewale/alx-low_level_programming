fb24bf2d98d820012229056d2bd5a62aa4aeeb70
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-11 19:18:12 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - program's main function.
+* Description: this is where program execution begins and ends.
+* @void: accepts no param.
+* Return: return value for main function
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
