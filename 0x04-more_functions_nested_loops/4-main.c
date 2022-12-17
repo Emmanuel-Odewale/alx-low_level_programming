9c7cebef39cec884fcb60bd61e0f1e566fbcd298
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-12-16 17:13:43 -0800

    i believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: main program function.
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number 0 - 9 except 2 and 4..
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
