432abaf934cf9e884019f11cbab79fcbee1dd905
Emmanuel Odewale <odebzemmygee@gmail.com>
2023-01-10 09:22:07 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, length;
+	int i, j;
 
-	i = 0;
-	length = strlen(src);
-	while (src[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for  (j = 0; src[j] != '\0'; j++)
 	{
-		dest[length] = src[i];
-		length += 1;
-		i += 1;
+		dest[i + j] = src[j];
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
