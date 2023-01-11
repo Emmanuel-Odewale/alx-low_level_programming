999ddf1d0fdc1833e0545edb15dd10fd4da1b91a
Emmanuel Odewale <odebzemmygee@gmail.com>
2023-01-11 08:03:42 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,9 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, rtn;
-
+	
 	rtn = 0;
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' ||  s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
