0a1e064bb3d7d908e831468b23c4e6a94fbedef1
Emmanuel Odewale <odebzemmygee@gmail.com>
2023-01-12 17:40:32 -0800

    expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,14 +5,25 @@
  * @str: string to capitalize
  * Return: pointer to string
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i;
 
 	i = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i - 1] == (' ' || '	' || ',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{') || (str[i - 2] == '\' && str[i - 1] == 'n'))
+		if (str[i - 1] >= 32 && str[i - 1] <= 34
+				|| str[i - 1] == 40
+				|| str[i - 1] == 41
+				|| str[i - 1] == 44
+				|| str[i - 1] == 46
+				|| str[i - 1] == 59
+				|| str[i - 1] == 63
+				|| str[i - 1] == 10
+				|| str[i - 1] == 9
+				|| str[i - 1] == 123
+				|| str[i - 1] == 125
+				)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
